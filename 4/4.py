92c76ce889902a586ea3595719a2042303470850
TheMonkeyz <rockmonkeyz@gmail.com>
2015-12-22 19:28:26 -0500

    Made faster by not displaying progress

--- 4/4.py
+++ 4/4.py
@@ -7,7 +7,7 @@ for i in range (999999):
 	hash = hashlib.md5("ckczppom" + str(i)).hexdigest()
 #	print str(i) + ': ' + hash 
 	stripped = str(hash[:5])
-	print str(i) + ':' + stripped
+#	print str(i) + ':' + stripped
 	if stripped == "00000":
 		print "Answer is: " + str(i)
 		break
